48865b3d1e0bebf996fd599f5a105720162ae3f4
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2024-12-03 17:56:41 +0800

    made changes

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import postLogin from "../api/post-login.js";
+import useAuth from "../hooks/use-auth.js";
 
 function LoginForm() {
+  const navigate = useNavigate();
+  const { auth, setAuth } = useAuth();
+
   const [credentials, setCredentials] = useState({
     username: "",
     password: "",
@@ -19,8 +24,12 @@ function LoginForm() {
   const handleSubmit = (event) => {
     event.preventDefault();
     if (credentials.username && credentials.password) {
-      postLogin().then((response) => {
+      postLogin(credentials.username, credentials.password).then((response) => {
         window.localStorage.setItem("token", response.token);
+        setAuth({
+          token: response.token,
+        });
+        navigate("/");
       });
     }
   };
