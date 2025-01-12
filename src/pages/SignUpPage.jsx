c915b5aeb7fd7fc9c1a28eda66c2c000b48d1b47
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2025-01-12 17:25:14 +0800

    added css elements to SignUpPage.css

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -6,8 +6,10 @@ function SignUpPage() {
   return (
     <div className="signup-container">
       <h1>Create a New Account</h1>
+      <div className="signup-form-container">
       <SignUpForm /> {/* Render the SignUpForm component */}
     </div>
+  </div>
   );
 }
 
