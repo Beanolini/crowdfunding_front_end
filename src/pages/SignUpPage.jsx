9d404c90d66393a4465887c4614d6923fa0b4098
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2025-01-12 19:09:13 +0800

    made css changes to signup page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import SignUpForm from "../components/SignUpForm"; 
-import "../components/SignUpForm.css"; 
+import SignUpForm from "../components/SignUpForm";
+import "../components/SignUpForm.css";
 
 function SignUpPage() {
   return (
     <div className="signup-container">
-      <h1>Create a New Account</h1>
       <div className="signup-form-container">
+        <h1>Create a New Account</h1>{" "}
+        {/* Move this heading inside the form container */}
         <SignUpForm /> {/* Render the SignUpForm component */}
       </div>
     </div>
