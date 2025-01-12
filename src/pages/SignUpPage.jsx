6cd80309808b6f5a9b7fe878c5a82e664f9bbc6d
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2025-01-12 18:20:24 +0800

    Fixed import paths for SignUpForm

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import SignUpForm from "./SignUpForm.jsx";
-import "./SignUpPage.css";
+import SignUpForm from "../components/SignUpForm"; 
+import "../components/SignUpForm.css"; 
 
 function SignUpPage() {
   return (
     <div className="signup-container">
       <h1>Create a New Account</h1>
       <div className="signup-form-container">
-      <SignUpForm /> {/* Render the SignUpForm component */}
+        <SignUpForm /> {/* Render the SignUpForm component */}
+      </div>
     </div>
-  </div>
   );
 }
 
